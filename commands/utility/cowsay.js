e6e1033a90ccf0a477cadaf1129758f7f8b4a2c4
Obdulio Benitez Garcia <112982014+OBenitezG@users.noreply.github.com>
2023-12-13 20:43:18 -0500

    part 4 saving point

---
+++ commands/utility/cowsay.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require('discord.js');
+const cowsay = require('cowsay');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('cowsay')
+    .setDescription('Cowsay: Input text to make a cow say it!')
+    .addStringOption((option) => option.setName('text')
+      .setDescription('The message to display')
+      .setRequired(true)
+      .setMaxLength(25))
+    .addStringOption((option) => option.setName('cow')
+      .setDescription('Which cow says it')
+      .setRequired(true)),
+  async execute(interaction) {
+    const cowText = interaction.options.getString('text');
+    const cowChoice = interaction.options.getString('cow');
+
+    const cowMessage = cowsay.say({
+      text: cowText,
+      cow: cowChoice,
+    });
+
+    await interaction.reply(`\`\`\`${cowMessage}\`\`\``);
+  },
+};
